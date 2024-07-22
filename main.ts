222bd50f2924b6d41879ede4855756af874969ae
pomdtr <achille.lacoin@gmail.com>
2024-07-22 16:51:47 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+const handler = (_req: Request) => {
+    return new Response("Hello from smallweb!", {
+        headers: { "content-type": "text/plain" },
+    });
+};
+
+export default {
+    fetch: handler,
+};
